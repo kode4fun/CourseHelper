a7e2ea5cbbbe3c5a78c8510135138fa1b5c275c7
kode4fun <kode4fun@proton.me>
2024-07-12 19:12:08 +0800

    重置文件

---
+++ docs/assets/NoMobileAccessView-DdbcQ0wm.js
@@ -0,0 +1,1 @@
+import"./index-DdIp2ATX.js";import{E as e}from"./el-message-box-mLy_XQI-.js";import"./el-input-CHcDLTp6.js";import"./el-overlay-BddMkEe_.js";const p={__name:"NoMobileAccessView",setup(r){return e.alert("请使用电脑端浏览器访问本程序！","错误",{type:"error"}).finally(()=>{window.close()}),(o,t)=>null}};export{p as default};
