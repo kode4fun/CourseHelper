05f964e67e9b0a88b297fdcb6bc67fbfa95db97c
kode4fun <kode4fun@proton.me>
2024-07-13 19:53:37 +0800

    修正逻辑错误

---
+++ docs/assets/NoMobileAccessView-DC4tiw0l.js
@@ -0,0 +1,1 @@
+import"./index-BfrW6-gN.js";import{E as e}from"./el-message-box-Cgj4YdrX.js";import"./el-input-BPX3NGSa.js";import"./el-overlay-P1xT7ull.js";const p={__name:"NoMobileAccessView",setup(r){return e.alert("请使用电脑端浏览器访问本程序！","错误",{type:"error"}).finally(()=>{window.close()}),(o,t)=>null}};export{p as default};
