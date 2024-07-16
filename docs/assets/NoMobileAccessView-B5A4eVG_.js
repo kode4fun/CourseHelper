780019dd1fb04730d7405f8331631eec4c67369b
kode4fun <kode4fun@proton.me>
2024-07-16 20:00:57 +0800

    fix:个别细节

---
+++ docs/assets/NoMobileAccessView-B5A4eVG_.js
@@ -0,0 +1,1 @@
+import"./index-BTU7nzG8.js";import{E as e}from"./el-message-box-BpST8eYI.js";import"./el-input-2qJAHRTu.js";import"./el-overlay-DNjSd9Ap.js";const p={__name:"NoMobileAccessView",setup(r){return e.alert("请使用电脑端浏览器访问本程序！","错误",{type:"error"}).finally(()=>{window.close()}),(o,t)=>null}};export{p as default};
