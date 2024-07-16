c7e16e01ebc43198b6a70040e4768ccc236b0a9b
kode4fun <kode4fun@proton.me>
2024-07-16 15:57:04 +0800

    增加新功能

---
+++ docs/assets/NoMobileAccessView-qJrk1jby.js
@@ -0,0 +1,1 @@
+import"./index-Bs3XcpzB.js";import{E as e}from"./el-message-box-6Ga1eIEv.js";import"./el-input-m6Jtdqql.js";import"./el-overlay-DU-HIqxq.js";const p={__name:"NoMobileAccessView",setup(r){return e.alert("请使用电脑端浏览器访问本程序！","错误",{type:"error"}).finally(()=>{window.close()}),(o,t)=>null}};export{p as default};
